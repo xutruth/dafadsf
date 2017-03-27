335b3e452509852be13eabb72b36a1bbbf922298
xutruth <1210062377@qq.com>
2017-03-27 15:25:19 +0800

    Initial commit

---
+++ src/main/java/ghb/myServlet.java
@@ -0,0 +1,17 @@
+package ghb;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by ASUS on 2017/3/7.
+ */
+public class myServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println(11111);
+    }
+}
