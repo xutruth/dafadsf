6e5decc55ae614b33e7419a87fa8968e0d75a6c4
xutruth <1210062377@qq.com>
2017-03-29 00:49:38 +0800

    退回

--- src/main/java/com/ghb/controller/HelloMvcController.java
+++ src/main/java/com/ghb/controller/HelloMvcController.java
@@ -10,10 +10,16 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class HelloMvcController {
     //localHost:8080/hello/mvc
-    @RequestMapping(value = "/mvc", method = RequestMethod.GET)
-    public String helloMvc() {
+    @RequestMapping(value = "/mvc",method = RequestMethod.GET)
+    public String helloMvc(){
         System.out.println(111111);
 
-        return "hello";
+        return "hellosdf";
+    }
+    @RequestMapping(value = "/mvc",method = RequestMethod.GET)
+    public String helloMvc111(){
+        System.out.println(111111);
+
+        return "hellosdf";
     }
 }
